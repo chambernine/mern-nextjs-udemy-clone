a8f136fcb13369dee8d3c4ff1595f507715d3841
chakorn.san <chakorn.san@cpf.co.th>
2024-06-19 19:12:09 +0700

    add Topbar

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,5 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Search } from "lucide-react";
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export default function TopBar() {
-  return <div>TopBar</div>;
+  const { isSignedIn } = useAuth();
+  console.log(isSignedIn);
+
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const topRoutes = [
+    { label: "Instructor", path: "/instructor/courses" },
+    { label: "Learning", path: "/learning" },
+  ];
+
+  const sidebarRoutes = [
+    { label: "Courses", path: "/instructor/courses" },
+    {
+      label: "Performance",
+      path: "/instructor/performance",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center p-4">
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={200} height={100} />
+      </Link>
+      <div className="max-md:hidden md:w-[400px] rounded-full flex">
+        <input
+          className="flex-grow bg-[#FFF8EB] rounded-l-full border-none outline-none text-sm pl-4 py-3"
+          placeholder="Search for courses"
+        />
+        <button
+          className="bg-[#FDAB04] rounded-r-full border-none outline-none cursor-pointer px-4 py-3 hover:bg-[#FDAB04]/80"
+          // disabled={searchInput.trim() === ""}
+          // onClick={handleSearch}
+        >
+          <Search className="h-4 w-4" />
+        </button>
+      </div>
+      <div className="flex gap-6 items-center">
+        <div className="max-sm:hidden flex gap-6">
+          {topRoutes.map((route) => (
+            <Link
+              href={route.path}
+              key={route.label}
+              className="text-sm font-medium hover:text-[#FDAB04]"
+            >
+              {route.label}
+            </Link>
+          ))}
+        </div>
+        {isSignedIn ? (
+          <UserButton afterSignOutUrl="/sign-in" />
+        ) : (
+          <Link href="/sign-in">
+            <Button>Sign In</Button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
