f3c7c70b5ecf480138f45441ce68ee6e2cb22b7a
chakorn.san <chakorn.san@axonstech.com>
2024-08-25 16:03:31 +0700

    feat : course table

--- app/(instructor)/instructor/courses/page.tsx
+++ app/(instructor)/instructor/courses/page.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
+import { DataTable } from "@/components/custom/DataTable";
+import { columns } from "@/components/courses/Columns";
 export default async function CoursesPage() {
   const { userId } = auth();
 
@@ -25,17 +27,10 @@ export default async function CoursesPage() {
     <div className="px-6 py-4">
       <Link href="/instructor/create-course">
         <Button> Create New Course</Button>
-        <div className="mt-10">
-          {courses.map((course) => (
-            <Link
-              key={course.id}
-              href={`/instructor/courses/${course.id}/basic`}
-            >
-              {course.title}
-            </Link>
-          ))}
-        </div>
       </Link>
+      <div className="mt-5">
+        <DataTable columns={columns} data={courses} />
+      </div>
     </div>
   );
 }
